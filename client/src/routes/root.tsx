b037968787860ce571a9431b32ae3b68055d9c96
Chris <chrisalexander0617@gmail.com>
2023-03-14 20:47:33 -0400

    Added react router

---
+++ client/src/routes/root.tsx
@@ -0,0 +1,28 @@
+import React, {useEffect, useState} from 'react';
+import '../App.css';
+import { ProfileAPI } from '../api/services/Profiles';
+import {ProfileType} from '../../src/types/Profile/index'
+import { ProfileList } from '../components/Profile/ProfileList';
+
+export const Root = () => {
+  const [profiles, setProfiles] = useState<ProfileType[]>([]);
+  const [loading, isLoading] = useState<boolean>(true);
+
+  const loadProfiles = async () => {
+    const result = await ProfileAPI.ListProfiles()
+    setProfiles(result)
+    return result
+  }
+
+  useEffect(() => {
+   loadProfiles()
+  },[loading])
+
+  if(!profiles.length) return <div>Loading...</div>
+
+  return (
+    <div className="App">
+      <ProfileList profiles={profiles}  />
+    </div>
+  );
+}
