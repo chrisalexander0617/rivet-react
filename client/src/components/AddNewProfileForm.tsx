c30457e031c224afe5a46fc681fec4a927439908
Chris <chrisalexander0617@gmail.com>
2023-03-16 22:44:05 -0400

    Added fucntion to check if string is URL

--- client/src/components/AddNewProfileForm.tsx
+++ client/src/components/AddNewProfileForm.tsx
@@ -1,158 +1,157 @@
 import { useState } from 'react'
 import { Box, Button, TextField, Typography } from '@mui/material'
-import { ProfileType, NewProfileType, FieldDataType } from '../types/Profile'
+import { NewProfileType, FieldDataType } from '../types/Profile'
 import { ProfileAPI } from '../api/services/Profiles'
 
 export const AddNewProfileForm = () => {
-  const [firstNameValue, setFirstNameValue] = useState<string>("")
-  const [lastNameValue, setLastNameValue] = useState<string>("")
-  const [phoneValue, setPhoneValue] = useState<string>("")
-  const [emailValue, setEmailValue] = useState<string>("")
-  const [addressValue, setAddressValue] = useState<string>("")
-  const [cityValue, setCityValue] = useState<string>("")
-  const [stateValue, setStateValue] = useState<string>("")
-  const [zipValue, setZipValue] = useState<string>("")
-  const [photoValue, setPhotoValue] = useState<string>("")
-  const [notesValue, setNotesValue] = useState<string>("")
+	const [firstNameValue, setFirstNameValue] = useState<string>("")
+	const [lastNameValue, setLastNameValue] = useState<string>("")
+	const [phoneValue, setPhoneValue] = useState<string>("")
+	const [emailValue, setEmailValue] = useState<string>("")
+	const [addressValue, setAddressValue] = useState<string>("")
+	const [cityValue, setCityValue] = useState<string>("")
+	const [stateValue, setStateValue] = useState<string>("")
+	const [zipValue, setZipValue] = useState<string>("")
+	const [photoValue, setPhotoValue] = useState<string>("")
+	const [notesValue, setNotesValue] = useState<string>("")
 
-  const newProfile: NewProfileType = {
-    first_name: firstNameValue,
-    last_name: lastNameValue,
-    phone: phoneValue,
-    email: emailValue,
-    address: addressValue,
-    city: cityValue,
-    state: stateValue,
-    zip: zipValue,
-    photo: photoValue,
-    notes: notesValue
-  }
+	const newProfile: NewProfileType = {
+		first_name: firstNameValue,
+		last_name: lastNameValue,
+		phone: phoneValue,
+		email: emailValue,
+		address: addressValue,
+		city: cityValue,
+		state: stateValue,
+		zip: zipValue,
+		photo: photoValue,
+		notes: notesValue
+	}
 
-  const handleAddNewProfile = async () => {
-    console.log('new Profile', newProfile)
-    try {
-      const result = await ProfileAPI.createNewProfile(newProfile)
-      console.log('result', result)
-    } catch (err) {
-      console.log(err, 'Something went wrong as hell')
-    }
-  }
+	const handleAddNewProfile = async () => {
+		console.log('new Profile', newProfile)
+		try {
+			const result = await ProfileAPI.createNewProfile(newProfile)
+			console.log('result', result)
+		} catch (err) {
+			console.log(err, 'Something went wrong as hell')
+		}
+	}
 
+	const fieldData: FieldDataType[] = [
+		{
+			label: 'first name',
+			value: firstNameValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setFirstNameValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'last name',
+			value: lastNameValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setLastNameValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'phone',
+			value: phoneValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setPhoneValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'email',
+			value: emailValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setEmailValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'address',
+			value: addressValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setAddressValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'city',
+			value: cityValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setCityValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'state',
+			value: stateValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setStateValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'zip',
+			value: zipValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setZipValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'photo',
+			value: photoValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setPhotoValue(e.currentTarget.value)
+			}
+		},
+		{
+			label: 'notes',
+			value: notesValue,
+			maxLength: 255,
+			required: true,
+			handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
+				setNotesValue(e.currentTarget.value)
+			}
+		},
+	]
 
-  const fieldData: FieldDataType[] = [
-    {
-      label: 'first name',
-      value: firstNameValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setFirstNameValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'last name',
-      value: lastNameValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setLastNameValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'phone',
-      value: phoneValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setPhoneValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'email',
-      value: emailValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setEmailValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'address',
-      value: addressValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setAddressValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'city',
-      value: cityValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setCityValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'state',
-      value: stateValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setStateValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'zip',
-      value: zipValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setZipValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'photo',
-      value: photoValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setPhotoValue(e.currentTarget.value)
-      }
-    },
-    {
-      label: 'notes',
-      value: notesValue,
-      maxLength: 255,
-      required: true,
-      handleStateChange: (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setNotesValue(e.currentTarget.value)
-      }
-    },
-  ]
+	const styles = {
+		display: 'flex',
+		flexDirection: 'column',
+		width: '400px',
+		margin: '0 auto',
+		gap: 1
+	}
 
-  const styles = {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '400px',
-    margin: '0 auto',
-    gap: 1
-  }
-
-  return (
-    <>
-      <Box sx={styles}>
-        {fieldData.map((item, i) => (
-          <TextField
-            label={item.label}
-            onChange={item.handleStateChange}
-            value={item.value}
-            inputProps={{ maxLength: item.maxLength }}
-          />
-        ))}
-        <Button onClick={handleAddNewProfile}>Update</Button>
-      </Box>
-    </>
-  )
+	return (
+		<>
+			<Box sx={styles}>
+				{fieldData.map((item, i) => (
+					<TextField
+						label={item.label}
+						onChange={item.handleStateChange}
+						value={item.value}
+						inputProps={{ maxLength: item.maxLength }}
+					/>
+				))}
+				<Button onClick={handleAddNewProfile}>Update</Button>
+			</Box>
+		</>
+	)
 }
--- client/src/components/Profile/ProfileList.tsx
+++ client/src/components/Profile/ProfileList.tsx
@@ -6,23 +6,53 @@ import { Link } from 'react-router-dom'
 export const ProfileList: React.FC<ProfileListType> = ({ profiles }) => {
   const styles = {
     profileCard: {
-      backgroundColor: '#efefef',
+      backgroundColor: '#f8f8f8',
       textAlign: 'left',
-      textDecoration: 'none'
+      textDecoration: 'none',
+      border: 'solid 1px black'
     }
+  }
+
+  const isValidHttpUrl = (str: string) => {
+    let url;
+    try {
+      url = new URL(str);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
   }
 
   return (
     <Container>
-      <Grid spacing={1} container>
+      <Grid spacing={3} container>
         {profiles.map((profile, i) => (
-          <Grid item xs={4} md={3}>
+          <Grid item xs={3} md={4}>
             <Link to={`/profile/${profile.id.toString()}`}>
               <Box sx={styles.profileCard} p={3}>
-                <div key={i}>
-                  <Typography color="black" variant="h5">{profile.first_name}</Typography>
-                  <Typography color="black" variant="h5">{profile.last_name}</Typography>
-                </div>
+                <Box sx={{
+                  display: 'flex',
+                  alignItems: 'center'
+                }} key={i}>
+                  <Box
+                    component="img"
+                    sx={{
+                      height: 50,
+                      width: 50,
+                      borderRadius: '100%',
+                      maxHeight: { xs: 233, md: 233 },
+                      maxWidth: { xs: 233, md: 233 },
+                      objectFit: 'cover',
+                      marginRight: 3,
+                    }}
+                    alt="No Image"
+                    src={isValidHttpUrl(profile.photo) ? profile.photo : 'https://images.unsplash.com/photo-1670272504471-61a632484750?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'}
+                  />
+                  <Box>
+                    <Typography color="black" variant="h5">{profile.first_name}</Typography>
+                    <Typography color="black" variant="h5">{profile.last_name}</Typography>
+                  </Box>
+                </Box>
               </Box>
             </Link>
           </Grid>
