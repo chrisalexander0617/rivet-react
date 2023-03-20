0d7adeb63e22f754ea96668e45bd85c82cdadf33
Chris <chrisalexander0617@gmail.com>
2023-03-20 09:10:12 -0400

    Clean up

--- client/src/routes/root.tsx
+++ client/src/routes/root.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import '../App.css';
-import {Box} from '@mui/material'
-import {Navbar} from '../components/NavBar/NavBar'
+import { Box } from '@mui/material'
+import { Navbar } from '../components/NavBar/NavBar'
 import { ProfileAPI } from '../api/services/Profiles';
 import { ProfileType } from '../../src/types/Profile/index'
 import { AddNewProfileForm } from '../components/AddNewProfileForm';
@@ -19,16 +19,16 @@ export const Root = () => {
     isLoading(false)
     return result
   }
-  
+
   const handleCloseModal = () => isModalOpen(false)
   const handleOpenModal = () => isModalOpen(true)
 
   const styles = {
-    FlexBoxColumn:{
-      display:'flex', 
-      alignItems:'left', 
-      flexDirection:'column', 
-      gap:3
+    FlexBoxColumn: {
+      display: 'flex',
+      alignItems: 'left',
+      flexDirection: 'column',
+      gap: 3
     }
   }
 
@@ -41,10 +41,10 @@ export const Root = () => {
   return (
     <Box p={1} className="App">
       <Navbar handleOpenModal={handleOpenModal} />
-        {modalOpen && <AddNewProfileForm handleCloseModal={handleCloseModal} />}
-        <Box sx={styles.FlexBoxColumn} p={3}>
-          <ProfileTable profiles={profiles} />
-        </Box>
+      {modalOpen && <AddNewProfileForm handleCloseModal={handleCloseModal} />}
+      <Box sx={styles.FlexBoxColumn} p={3}>
+        <ProfileTable profiles={profiles} />
+      </Box>
     </Box>
   );
 }
